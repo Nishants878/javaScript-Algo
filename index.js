83bb7052ac77223df96809e2c84fa2575ccb2d14
Nishants878 <nishants878@gmail.com>
2021-05-10 16:13:46 +0530

    fibonacci series

--- index.js
+++ index.js
@@ -20,6 +20,90 @@ const isPrime = (a) =>{
         }
     }
 }
-isPrime(4)
+isPrime(17)
 
 // code of a prime number
+
+// program to print prime numbers between the two numbers
+
+const primeInBetween = (b, b1) =>{
+    const b2 = parseInt(b);
+    const b3 = parseInt(b1);
+
+    for(let i=b2; i<=b3; i++){
+      let b4 = 0;
+
+      for(let j = 2; j<i; j++){
+          if(i%j == 0){
+              b4=1;
+              break;
+          }
+      }
+
+      if(i > 1 && b4 == 0){
+        console.log(i)
+    }
+    }
+     
+    
+
+}
+primeInBetween(2,17)
+ 
+// program to print prime numbers between the two numbers
+// print the first “n” prime numbers
+
+const firstPrimeNumber = (n) =>{
+    var primes=[2];
+    for (var i=2;primes.length<n;i++){
+        var prime=true;
+        var rootI=Math.sqrt(i)+1;
+        for (var j=2;j<rootI;j++){
+            if (i%j==0) {prime=false;break;}
+        };
+        if (prime) primes.push(i);
+    };
+    console.log(primes)
+}
+firstPrimeNumber(8)
+
+// print the first “n” prime numbers
+
+// program to generate fibonacci series up to n terms
+  
+const fibonacciTon = (c) =>{
+    const c1 = parseInt(c);
+    let c2 =0, c3=1, c4;
+
+    for(let i=1; i<=c1; i++){
+        console.log(c2);
+        c4= c2+c3;
+        c2=c3;
+        c3=c4;
+    }
+}
+fibonacciTon(4)
+
+// program to generate fibonacci series up to n terms
+
+
+
+// program to check whether the last digit of three numbers is same 
+  
+const checkLastDigitSame = (d,d1,d2) => {
+        // find the last digit
+        const resultD = d%10;
+        const resultD1 = d1%10;
+        const resultD2 = d2%10;
+
+        if(resultD == resultD1 && resultD == resultD2) {
+            console.log("same last digit")
+        }else{
+            console.log("not same")
+        }
+        
+}
+checkLastDigitSame(8,38,4)
+
+
+// program to check whether the last digit of three numbers is same 
