27c0098574482146c88f605efc4cbbb7ad064ba9
Nishant Kumar <nishants878@gmail.com>
2021-06-29 19:51:03 +0530

    phone number code

--- playground.js
+++ playground.js
@@ -59,4 +59,21 @@ function encryptThis (text){
    
  }
 }
-encryptThis("Hello all this is my first encryption")
+
+
+//Write a function that accepts an array of 10 integers (between 0 and 9), that returns a string of those numbers in the form of a phone number.
+// => returns "(123) 456-7890"
+
+
+function createPhoneNumber(numbers){
+     if(numbers.length !== 10){
+         return console.log(false)
+     }
+     else{
+         let filteringFirstThree = numbers.splice(0, 3)
+
+         console.log(filteringFirstThree)
+
+     }
+}
+createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0])
